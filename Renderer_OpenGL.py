567483fdce22e71763da3621c1c290a97d8a9ce3
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-11-14 11:50:47 -0600

    two modelss

--- Renderer_OpenGL.py
+++ Renderer_OpenGL.py
@@ -1,13 +1,11 @@
 from pickle import TRUE
 import pygame
 from pygame.locals import *
-
 from shaders import *
-
 from gl import Renderer, Model
 
-from math import cos, sin, radians
 
+pygame.display.set_caption('Proyecto OGL - Estefania Elvira Ramos')
 width = 960
 height = 540
 
@@ -36,10 +34,19 @@ rend.scene.append( face )
 
 
 isRunning = True
+isStop = False
+
+zoom = 50
+ladox = 200
+ladoy = 200
+
 
 while isRunning:
 
     keys = pygame.key.get_pressed()
+    opcion1 = keys[K_LSHIFT] and keys[K_1]
+    opcion2 = keys[K_LSHIFT] and keys[K_2]
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,48 +55,76 @@ while isRunning:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
-
-            elif event.key == pygame.K_z:
-                rend.filledMode()
-            elif event.key == pygame.K_x:
-                rend.wireframeMode()
-
-    if keys[K_q]:
-        if rend.camDistance > 2:
-            rend.camDistance -= 2 * deltaTime
-    elif keys[K_e]:
-        if rend.camDistance < 10:
-            rend.camDistance += 2 * deltaTime
-
-    if keys[K_a]:
-        rend.angle -= 30 * deltaTime
-    elif keys[K_d]:
-        rend.angle += 30 * deltaTime
-
-
-    if keys[K_w]:
-        if rend.camPosition.y < 2:
-            rend.camPosition.y += 5 * deltaTime
-    elif keys[K_s]:
-        if rend.camPosition.y > -2:
-            rend.camPosition.y -= 5 * deltaTime
-
-
-    rend.target.y = rend.camPosition.y
-
-    rend.camPosition.x = rend.target.x + sin(radians(rend.angle)) * rend.camDistance
-    rend.camPosition.z = rend.target.z + cos(radians(rend.angle)) * rend.camDistance
-    
+                    
+    ##Movimiento  de las camaras en el eje Y y X
     if keys[K_LEFT]:
-        rend.pointLight.x -= 10 * deltaTime
+        if ladox > 0:
+            rend.camPosition.x -= 10 * deltaTime
+            ladox -= 10
     elif keys[K_RIGHT]:
-        rend.pointLight.x += 10 * deltaTime
+        if ladox <= 360:
+            rend.camPosition.x += 10 * deltaTime
+            ladox += 10
     elif keys[K_UP]:
-        rend.pointLight.y += 10 * deltaTime
+        if ladoy <= 360:
+            rend.camPosition.y += 10 * deltaTime
+            ladoy += 10
     elif keys[K_DOWN]:
-        rend.pointLight.y -= 10 * deltaTime
+        if ladoy > 0:
+            rend.camPosition.y -= 10 * deltaTime
+            ladoy -= 10
+    ##Zoom de las camaras y alejar
+    if keys[K_z]:
+        if zoom <= 100:
+            rend.camPosition.z -= 10  * deltaTime
+            zoom += 1
+    elif keys[K_a]:
+        if zoom > 0:
+            rend.camPosition.z += 10 * deltaTime
+            zoom -= 1
+    
+    ##Zoom con el mouse
+    if event.type == pygame.MOUSEWHEEL:
+            if event.y == 1:
+                if zoom <= 100:
+                    rend.camPosition.z -= 50 * deltaTime
+                    zoom += 10
+            elif event.y == -1:
+                if zoom > 0:
+                    rend.camPosition.z += 50*deltaTime
+                    zoom -= 10
+    ##SHADERSSSS
+    if keys[K_1]: 
+        rend.setShaders(vertex_shader, fragment_shader)
+
+    elif keys[K_2]: 
+        rend.setShaders(vertex_shader, toon_shader)
+    
 
 
+    ##MENU
+    if opcion1:
+        rend.scene.clear()
+        face = Model("cala.obj", "cala.bmp")
+
+        face.position.z -= 5
+        face.scale.x = 2
+        face.scale.y = 2
+        face.scale.z = 2
+
+        rend.scene.append( face )
+
+    elif opcion2:
+        rend.scene.clear()
+        face = Model("mask.obj", "mask.bmp")
+
+        face.position.z -= 5
+        face.scale.x = 2
+        face.scale.y = 2
+        face.scale.z = 2
+
+        rend.scene.append( face )
+    
     deltaTime = clock.tick(60) / 1000
     rend.time += deltaTime
 
